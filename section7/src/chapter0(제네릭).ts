5d72baca173e973ff6ac32fe034e950a0bfc9c0e
BigMango97 <mgk04222@naver.com>
2023-07-06 20:05:49 +0900

    제네릭

--- section7/src/chapter0(제네릭).ts
+++ section7/src/chapter0(제네릭).ts
@@ -4,6 +4,9 @@
  */
 
 // 제네릭 함수
+// 함수의 인수에 따라서 반환값 타입을 가변적으로 정해줄수있음
+// <T> : 타입변수 - 타입을 저장하는 변수
+// 함수를 호출할때 인수에 따라서 타입이 정해짐!
 function func<T>(value: T): T {
   return value;
 }
@@ -11,5 +14,5 @@ function func<T>(value: T): T {
 let num = func(10);
 let bool = func(true);
 let str = func("string");
-
+// 타입을 명시적으로 작성할수도 있음
 let arr = func<[number, number, number]>([1, 2, 3]);
