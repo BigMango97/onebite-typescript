ecf96c5198514f4b3c35113737703d7a6ca410bf
BigMango97 <mgk04222@naver.com>
2023-07-06 20:06:16 +0900

    map, forEach 메서드타입 정의하기

--- section7/src/chapter2(map,forEach메서드타입정의하기).ts
+++ section7/src/chapter2(map,forEach메서드타입정의하기).ts
@@ -1,8 +1,8 @@
 /**map메서드 : 새로운 배열 반환*/
 const arr = [1, 2, 3];
-const newArr = arr.map((it) => it * 2);
+const newArr = arr.map((it) => it * 2); // [2,4,6]
 
-function map<T, U>(arr: T[], callback: (item: T) => U) {
+function map<T, U>(arr: T[], callback: (itme: T) => U) {
   let result = [];
   for (let i = 0; i < arr.length; i++) {
     result.push(callback(arr[i]));
@@ -10,9 +10,11 @@ function map<T, U>(arr: T[], callback: (item: T) => U) {
   return result;
 }
 map(arr, (it) => it * 2);
+// 스트링 타입을 인수로 전달한다고 해서 결과값이 string일 이유는 없다!
 map(["hi", "hello"], (it) => parseInt(it));
 
-/**forEach : 원본 변경*/
+
+// forEach : 원본 변경
 const arr2 = [1, 2, 3];
 arr2.forEach((it) => console.log(it));
 
@@ -23,4 +25,7 @@ function forEach<T>(arr: T[], callback: (item: T) => void) {
 }
 forEach(arr2, (it) => {
   console.log(it.toFixed());
+});
+forEach(['123', '456'], (it) => {
+  console.log(it);
 });
